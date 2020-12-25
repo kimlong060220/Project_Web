0bd98059cf89a7198551a9b5e26751b5cdff7908
LENGHIA-CN8 <48158941+LENGHIA-CN8@users.noreply.github.com>
2020-12-25 21:47:53 +0700

    push heroku 25/12 2

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,12 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 const __dirname = path.resolve();
-if(process.env.NODE_ENV === 'production') {
+// if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*',(req,res) =>{
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
   })
-}
+// }
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
